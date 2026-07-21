96d31dd2446d3cb7022e996a8819f54d72b86b2f
ivanishvilil774-dot <ivanishvilil774@gmail.com>
2026-07-21 19:41:26 +0300

    day182

---
+++ day182/homework/ecommerce/main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    fullname = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
